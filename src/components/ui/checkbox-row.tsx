ffa61bd5ec16ffa9d9c06ff6858d79f00862ee9e
Justin Jones <jjones@costar.com>
2025-07-16 13:48:45 -0400

    added button onclick

--- src/components/ui/checkbox-row.tsx
+++ src/components/ui/checkbox-row.tsx
@@ -1,21 +1,25 @@
-import { useRef, useEffect, useContext } from "react";
+import { useRef, useEffect, useContext, useState } from "react";
 import { Status } from "@/types/types";
 
 
 export type TrackerCheckboxProps = {
     status: Status;
+    onClick?: () => void;
 }
 
 const TrackerCheckbox = ({
     status = Status.NOT_COMPLETED,
+    onClick,
 }: TrackerCheckboxProps) => {
     return (
         <button className="
         text-blue-500 hover:text-blue-700 
-        focus:outline-none focus:ring-2 focus:ring-blue-500">
+        focus:outline-none focus:ring-2 focus:ring-blue-500"
+        onClick={onClick}
+        >
         {
             status === "completed" ? "✔️" :
-            status === "skipped" ? "➖" :
+            status === "skipped" ? "⏭️" :
             status === "not_completed" ? "❌" :
             "?"
         }
@@ -31,14 +35,32 @@ export const CheckboxRow = (
     {num}: ButtonRowProps
 ) => {
     const buttons = [...Array(num).keys()];
+    const [status, setStatus] = useState<Status[]>(Array(num).fill(Status.NOT_COMPLETED));
+
+    const handleCheckboxClick = (index: number) => {
+        setStatus((prevStatus) => {
+            const newStatus = [...prevStatus];
+            switch (newStatus[index]) {
+                case Status.NOT_COMPLETED:
+                    newStatus[index] = Status.SKIPPED;
+                    break;
+                case Status.SKIPPED:
+                    newStatus[index] = Status.COMPLETED;
+                    break;
+                default:
+                    newStatus[index] = Status.NOT_COMPLETED;
+                    break;
+            }
+            return newStatus;
+        });
+    }
 
     return (
         <div
-            className="flex-2 flex justify-between items-center mx-4 my-1"
-            style={{ minWidth: 0 }}
+            className="flex justify-between items-center mx-4 my-1"
         >
             {buttons.map((button) => (
-                <TrackerCheckbox status={Status.NOT_COMPLETED} key={button} />
+                <TrackerCheckbox status={status[button]} key={button} onClick={() => handleCheckboxClick(button)} />
             ))}
         </div>
     );
