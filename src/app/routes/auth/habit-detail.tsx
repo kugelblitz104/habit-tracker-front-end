bf0a4b51150e4685ce00a16aeabbaa3da53b19ad
Justin Jones <jjones@costar.com>
2025-12-18 15:35:26 -0500

    string validation on habit detail view

--- src/app/routes/auth/habit-detail.tsx
+++ src/app/routes/auth/habit-detail.tsx
@@ -1,6 +1,7 @@
 import { HabitDetailView } from '@/components/layouts/habit-detail-view';
 import { ProtectedRoute } from '@/features/auth/components/protected-route';
 import type { Route } from './+types/home';
+import { ErrorScreen } from '@/components/layouts/error-screen';
 
 export function meta({}: Route.MetaArgs) {
     return [
@@ -12,9 +13,15 @@ export function meta({}: Route.MetaArgs) {
 export default function HabitDetail({
     params
 }: Route.ComponentProps & { params: { habitId: string } }) {
+    const habitId = parseInt(params.habitId, 10);
+
+    if (isNaN(habitId)) {
+        return <ErrorScreen message='Invalid habit ID' />;
+    }
+
     return (
         <ProtectedRoute>
-            <HabitDetailView habitId={Number(params.habitId)} />
+            <HabitDetailView habitId={habitId} />
         </ProtectedRoute>
     );
 }
