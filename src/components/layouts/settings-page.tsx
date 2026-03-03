a96fb249399bba72f874ad9c14d0205f8b2c43d8
Justin Jones <jjones@costar.com>
2026-03-03 14:47:29 -0500

    added handleUserUpdate for Settings

--- src/components/layouts/settings-page.tsx
+++ src/components/layouts/settings-page.tsx
@@ -1,14 +1,29 @@
-import { useAuth } from '@/lib/auth-context';
-import { PageShell } from '../ui/page-shell';
+import type { UserUpdate } from '@/api';
 import { UpdateUserForm } from '@/features/auth/components/update-user-form';
+import { updateUser } from '@/features/users/api/update-users';
+import { useAuth } from '@/lib/auth-context';
+import { useNavigate } from 'react-router';
+import { PageShell } from '../ui/page-shell';
 
 export const SettingsPage = () => {
-    const { user } = useAuth();
+    const { user, logout } = useAuth();
+    const navigate = useNavigate();
+
+    const handleUserUpdate = (updatedUser: UserUpdate) => {
+        updateUser(user!.id, updatedUser)
+            .then((response) => {
+                logout();
+                navigate('/login');
+            })
+            .catch((error) => {
+                console.error('Failed to update user:', error);
+            });
+    };
 
     return (
         <PageShell title='Settings'>
             <div className='m-4'>
-                <UpdateUserForm user={user!} />
+                <UpdateUserForm user={user!} handleUpdateUser={handleUserUpdate} />
             </div>
         </PageShell>
     );
