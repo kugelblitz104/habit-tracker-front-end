ff34448bddab9428a630406f4ba6bfc463f2aabd
Justin Jones <jjones@costar.com>
2026-03-10 15:57:01 -0400

    require habit information

--- src/features/habits/api/get-habits.ts
+++ src/features/habits/api/get-habits.ts
@@ -1,11 +1,11 @@
 import type { HabitList, HabitRead } from '@/api';
 import { HabitsService, UsersService } from '@/api';
 
-export const getHabits = async (userId = 1, limit = 100): Promise<HabitList> => {
+export const getHabits = async (userId: number, limit = 100): Promise<HabitList> => {
     return await UsersService.listUserHabitsUsersUserIdHabitsGet(userId, limit);
 };
 
-export const getHabit = async (habitId?: number): Promise<HabitRead> => {
+export const getHabit = async (habitId: number): Promise<HabitRead> => {
     if (!habitId) throw new Error('habitId is required');
     return await HabitsService.readHabitHabitsHabitIdGet(habitId);
 };
