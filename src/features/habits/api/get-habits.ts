62ff1cfd6a07bc2dfae68c9ed690b9ba6fbb4bea
Justin Jones <jjones@costar.com>
2026-03-10 10:59:20 -0400

    remove depreciated routes

--- src/features/habits/api/get-habits.ts
+++ src/features/habits/api/get-habits.ts
@@ -1,4 +1,4 @@
-import type { HabitKPIs, HabitList, HabitRead, Streak } from '@/api';
+import type { HabitList, HabitRead } from '@/api';
 import { HabitsService, UsersService } from '@/api';
 
 export const getHabits = async (userId = 1, limit = 100): Promise<HabitList> => {
@@ -8,14 +8,4 @@ export const getHabits = async (userId = 1, limit = 100): Promise<HabitList> => 
 export const getHabit = async (habitId?: number): Promise<HabitRead> => {
     if (!habitId) throw new Error('habitId is required');
     return await HabitsService.readHabitHabitsHabitIdGet(habitId);
-};
-
-export const getHabitKPIs = async (habitId?: number): Promise<HabitKPIs> => {
-    if (!habitId) throw new Error('habitId is required');
-    return await HabitsService.getHabitKpisHabitsHabitIdKpisGet(habitId);
-};
-
-export const getHabitStreaks = async (habitId?: number): Promise<Streak[]> => {
-    if (!habitId) throw new Error('habitId is required');
-    return await HabitsService.getHabitStreaksHabitsHabitIdStreaksGet(habitId);
 };
